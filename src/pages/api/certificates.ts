026ae18118aa6aa6fcec343b8f51467c33953d16
Ismail ZAHIR <ismailza407@gmail.com>
2024-09-29 13:30:04 +0100

    fixing unused variable

--- src/pages/api/certificates.ts
+++ src/pages/api/certificates.ts
@@ -6,6 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const projects = await getAllExperiences();
     res.status(200).json(projects);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: 'Error fetching certificates' });
   }
 }
--- src/pages/api/services.ts
+++ src/pages/api/services.ts
@@ -6,6 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const services = await getAllServices();
     res.status(200).json(services);
   } catch (error) {
+    console.error(error);
     res.status(500).json({ message: 'Error fetching services' });
   }
 }
