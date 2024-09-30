a077b626ee1e7cd8ad5bb51b54eb72c5b7bb330c
Ismail ZAHIR <ismailza407@gmail.com>
2024-09-30 21:57:02 +0100

    fixe favicon

--- src/config/app.ts
+++ src/config/app.ts
@@ -5,6 +5,19 @@ export const metadata: Metadata = {
   authors: [{ name: 'Ismail ZAHIR'}],
   description: "I'm Ismail ZAHIR, a Software Engineering and Computer Systems Integration student with a strong foundation in software engineering. I'm passionate about innovation and love exploring blockchain and artificial intelligence. Join me in creating exceptional software experiences and embracing the potential of technology.",
   keywords: ["Ismail ZAHIR", "Software Engineer", "Blockchain", "Artificial Intelligence", "Software Engineering", "Computer Systems Integration"],
+  icons: [
+    {
+      url: "/profile.svg",
+      type: "image/svg+xml",
+      rel: "icon",
+    },
+    {
+      url: "/profile.svg",
+      sizes: "192x192",
+      type: "image/svg+xml",
+      rel: "apple-touch-icon",
+    },
+  ],
 };
 
 export const logo = {
