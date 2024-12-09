ecaf5e9b5ac05c1f66803b0f7b858f45a55b3e9a
Ismail ZAHIR <ismailza407@gmail.com>
2024-12-09 11:14:52 +0100

    update resume

--- src/config/app.ts
+++ src/config/app.ts
@@ -26,8 +26,8 @@ export const logo = {
 };
 
 export const resumes = {
-  english: "https://drive.google.com/file/d/1S0uCrIynYSZTYCblbA9LCCvFhski4ozU/view?usp=drive_link",
-  french: "https://drive.google.com/file/d/100piM1SscuiZafWddHkjCyYyzWD1r7eu/view?usp=drive_link",
+  english: "https://drive.google.com/file/d/1VPeTRBMxlBFkbn7DpAY2-dZMt-xLlIf6/view?usp=drive_link",
+  french: "https://drive.google.com/file/d/177IZdFjZGbCGyPEfiK1fORis538jLfGE/view?usp=drive_link",
 }
 
 export const contact = {
