fc3c4e8d293868ccaacfa3bfba356fdf8ae66c8f
Ismail ZAHIR <ismail.zahir@somei.fr>
2025-03-04 22:52:24 +0000

    add new certificates

--- src/components/certificate.tsx
+++ src/components/certificate.tsx
@@ -4,7 +4,7 @@ const Certificate = ({ data: certificate }: { data: CertificateProps }) => {
   return (
     <div className="flex items-end overflow-hidden bg-cover rounded-lg h-64"
          style={{backgroundImage: `url(${certificate.image})`}}>
-      <div className="w-full px-8 py-4 overflow-hidden rounded-b-lg backdrop-blur-sm bg-white/60 dark:bg-gray-800/60">
+      <a href={certificate.link} target="_blank" className="w-full px-8 py-4 overflow-hidden rounded-b-lg backdrop-blur-sm bg-white/60 dark:bg-gray-800/60">
         <h2 className="mt-4 text-xl font-semibold text-gray-800 capitalize dark:text-white">
           {certificate.title}
         </h2>
@@ -14,9 +14,8 @@ const Certificate = ({ data: certificate }: { data: CertificateProps }) => {
           <p
             className="text-sm tracking-wider text-blue-500 uppercase dark:text-blue-300">{certificate.issuedAt}</p>
         </div>
-      </div>
+      </a>
     </div>
-
   );
 }
 
